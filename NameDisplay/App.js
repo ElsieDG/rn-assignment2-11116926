36b8c0097e04e59102951f05e563c50fa0201568
Elsie-EA-DGO <emeeade-graft-owoo@st.ug.edu.gh>
2024-05-26 18:20:39 +0000

    Changed background colour, changed text component content, changed font size

--- NameDisplay/App.js
+++ NameDisplay/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24 }}>My name is Elsie</Text>
+      <Text style={{ fontSize: 24 }}>My name is Elsi</Text>
       <StatusBar style="auto" />
     </View>
   );
