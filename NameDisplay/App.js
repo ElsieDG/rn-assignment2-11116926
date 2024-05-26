923995d00b71ab6ab54bfc60e1ba032c561ab43c
Elsie-EA-DGO <emeeade-graft-owoo@st.ug.edu.gh>
2024-05-26 18:26:02 +0000

    Made the name appear in bold font

--- NameDisplay/App.js
+++ NameDisplay/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24 }}>My name is Elsie</Text>
+      <Text style={{ fontSize: 24 }}>
+        My name is <Text style={{ fontWeight: 'bold' }}>Elsie</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
